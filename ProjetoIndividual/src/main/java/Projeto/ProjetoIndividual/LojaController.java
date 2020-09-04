c22d58247ffae1ebddc2d043da85cb05719fcb1f
PedroLucasdaSilva <pedro.lsilva@bandtec.com.br>
2020-09-04 11:31:49 -0300

    Tudo funcionando e mudança nos endpoints (ResponseEntity,etc)

--- ProjetoIndividual/src/main/java/Projeto/ProjetoIndividual/LojaController.java
+++ ProjetoIndividual/src/main/java/Projeto/ProjetoIndividual/LojaController.java
@@ -1,5 +1,6 @@
 package Projeto.ProjetoIndividual;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -13,34 +14,49 @@ public class LojaController {
     private List<Lucro> vendas =new ArrayList<>();
     //Lucravel l = new Lucravel();
 
-    @GetMapping("/listar")
-    public List<Lucro> getVendas(){
+    @GetMapping
+    public ResponseEntity getVendas(){
         //return l.getVendas();
-        return vendas;
+        if (vendas.isEmpty()){
+            return ResponseEntity.status(204).build();
+        }else{
+            return ResponseEntity.ok(vendas);
+        }
     }
 
-    @PostMapping("/registrar/filme")
-    public void registrarVendasFilmes(@RequestBody Filme f){
+    @PostMapping("/filme")
+    public ResponseEntity registrarVendasFilmes(@RequestBody Filme f){
         //l.addLucro(f);
         vendas.add(f);
+        return ResponseEntity.status(201).build();
     }
 
-    @PostMapping("/registrar/jogo")
-    public void registrarVendasJogos(@RequestBody Jogo j){
+    @PostMapping("/jogo")
+    public ResponseEntity registrarVendasJogos(@RequestBody Jogo j){
         //l.addLucro(j);
         vendas.add(j);
+        return ResponseEntity.status(201).build();
     }
 
-    @GetMapping("/listar/{id}")
-    public Lucro getLucro(@PathVariable int id){
+    @GetMapping("/{id}")
+    public ResponseEntity getLucro(@PathVariable int id){
         //return l.getVendas().get(id-1);
-        return vendas.get(id-1);
+        if(vendas.size()>=id){
+            return ResponseEntity.ok(vendas.get(id-1));
+        }else{
+            return ResponseEntity.status(404).build();
+        }
     }
 
-    @DeleteMapping("/deletar/{id}")
-    public void deleteLucro(@PathVariable int id){
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteLucro(@PathVariable int id){
         //l.getVendas().remove(id-1);
-        vendas.remove(id-1);
+        if(vendas.size()>=id){
+            vendas.remove(id-1);
+            return ResponseEntity.ok().build();
+        }else{
+            return ResponseEntity.status(404).build();
+        }
     }
 
 }
